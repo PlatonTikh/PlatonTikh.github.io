096a23ffe9b0ac6f5e1febc2e18f12fc10690341
Platon <plat.tikh@gmail.com>
2020-02-04 15:12:31 +0300

    new

--- exp.js
+++ exp.js
@@ -51,7 +51,7 @@ butt.addEventListener('click', function () {
   frstField.value = '';
   scndField.value = '';
   //
-  if (counter === allar.length) {
+  if (counter === allar.length - 1) {
     var finishButton = document.createElement("button" ); // создаем новый элемент <button>
 	  var text = document.createTextNode("Завершить" ); // создаем текстовое содержимое
 	  finishButton.appendChild(text); // добавляем текстовое содержимое элементу <button>
